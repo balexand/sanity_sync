a68444f27c0b059fa0c8b401d43c4fef022d187d
Brian Alexander <balexand@gmail.com>
2022-10-24 10:37:03 -0600

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule Sanity.Sync.MixProject do
       {:inflex, "~> 2.1"},
       {:jason, "~> 1.4"},
       {:postgrex, ">= 0.0.0"},
-      {:sanity, "~> 0.11"},
+      {:sanity, "~> 0.12"},
       {:unsafe_atomize_keys, "~> 1.1"},
 
       # Dev/test
